238344e821bcab5c77e58645b6fd900c41f446d7
ben476 <benjamin.hong476@gmail.com>
2022-07-14 02:16:24 +1200

    Initial commit

---
+++ setupdocker.sh
@@ -0,0 +1,14 @@
+cd /tmp
+apt remove docker-hypriot
+wget https://download.docker.com/linux/static/stable/aarch64/docker-20.10.9.tgz
+tar xzvf docker-20.10.9.tgz
+cp docker/* /usr/bin/
+echo "arm_64bit=1" >> /boot/config.txt
+cd /etc/systemd/system
+rm docker.service
+rm docker.socket
+wget https://github.com/moby/moby/raw/master/contrib/init/systemd/docker.service
+wget https://github.com/moby/moby/raw/master/contrib/init/systemd/docker.socket
+systemctl enable docker.service
+systemctl enable containerd.service
+reboot
